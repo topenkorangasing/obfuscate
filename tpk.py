6f4ad9fd1f5c92429b5540d5da87cbcae3a67d81
topenk <65455132+topenkorangasing@users.noreply.github.com>
2020-05-16 23:10:04 +0700

    Update tpk.py

--- tpk.py
+++ tpk.py
@@ -63,7 +63,7 @@ def enkrip():
    try:
        script = raw_input(ask + W + "Script " + G + "> " + W)
        output = raw_input(ask + W + "Output " + G + "> " + W)
-       os.system("bash-topsh " + script + " -o " + output )
+       os.system("bash-obfuscate " + script + " -o " + output )
        print (sukses + "Done..")
    except KeyboardInterrupt:
        print (eror + " Stopped!")
